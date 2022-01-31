e5d021d8e47645418c2cccfc8836165a4bf9a3a3
Vaibhav Jain <vaibhav@linux.ibm.com>
2022-01-31 02:27:06 -0600

    Updated the boot scripts

--- boot_mglru.sh
+++ boot_mglru.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 KERNEL_ARGS="systemd.unified_cgroup_hierarchy=1 root=UUID=b6b1ae58-7257-4b52-bd60-baedc42f39e4 transparent_hugepage=never"
-kexec -s --initrd ../linux/initrd-mglru.img ../linux/mglru-vmlinux --append="$KERNEL_ARGS"
+kexec -s --initrd ../linux/initrd-mglru.img ../linux/vmlinux-mglru --append="$KERNEL_ARGS $EXTRA_ARGS"
 kexec -e
